82fe75c159cb027a3e1e09d44ebd16b6d14e6dc8
Alexander Shangin <alarriva@yandex.ru>
2023-09-01 22:10:39 +0600

    Fix app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
